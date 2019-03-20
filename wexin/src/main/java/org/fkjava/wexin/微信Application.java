2c348f92d87831f497c50a6a9b65f58b237befb2
linjiatan <linjiatan@ljt>
2019-03-20 13:29:31 +0800

    初次提交Sring boot的项目模板

---
+++ wexin/src/main/java/org/fkjava/wexin/微信Application.java
@@ -0,0 +1,13 @@
+package org.fkjava.wexin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class 微信Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(微信Application.class, args);
+	}
+
+}
